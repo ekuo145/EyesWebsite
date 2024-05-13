9f93ba74f6a86a48e6d00e3b45b54f262833d6bd
Evan <kuoevan145@gmail.com>
2024-05-12 21:36:29 -0700

    Gotta get used to committing regularly

---
+++ CheatSheet.js
@@ -0,0 +1,8 @@
+function toggleCheatSheet() {
+    var cheatSheet = document.getElementById("cheatSheet");
+    if (cheatSheet.style.display === "none") {
+      cheatSheet.style.display = "block";
+    } else {
+      cheatSheet.style.display = "none";
+    }
+  }
